cd06a249481324fc4298207eb5f1b55555590b9b
unknown <lienapuey@gmail.com>
2023-03-08 16:13:14 +0100

    profile changes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
-  showNav = true;
+  constructor(private modal: NgbModal){}
+
 }
